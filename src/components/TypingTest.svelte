1f72a1f2d2753b6cf9b7d03fd32b26a761630d8e
7USTIN <justin.faak@icloud.com>
2021-09-16 17:37:31 +0200

    add restart test, tweaked styling

--- src/components/TypingTest.svelte
+++ src/components/TypingTest.svelte
@@ -4,7 +4,8 @@
 	interface char {
 		char: string,
 		hit: boolean,
-		error: boolean
+		error: boolean,
+		current: boolean
 	}
 
 	let words: char[][] = getRandomWords(20)
@@ -13,6 +14,15 @@
 
 	function getRandomItem(array: any[]): [] {
 		return array[array.length * Math.random() | 0]
+	}
+
+	function pushChar(char: string, error: boolean): char {
+		return {
+			char,
+			error,
+			hit: false,
+			current: false,
+		}
 	}
 
 	function getRandomWords(numWords: number): [][] { // TODO
@@ -27,15 +37,12 @@
 			array.push([])
 
 			for (let j = 0; j < words[i].length; j++) {
-				array[i].push({
-					char: words[i][j], hit: false, error: false,
-				})
+				array[i].push(pushChar(words[i][j], false))
 			}
+			array[0][0].current = true
 			
 			if (i + 1 !== numWords) {
-				array[i].push({
-					char: " ", hit: false, error: false 
-				})
+				array[i].push(pushChar(" ", false))
 			}
 		}
 
@@ -49,59 +56,61 @@
 	}
 
 	function handleKeydown(e: KeyboardEvent): void {
-		switch(e.key) {
-			case "Tab":
-				e.preventDefault()
-				reset()
-				break
-			default:
-				if (e.key === words[wordIdx][charIdx].char) {
-					words[wordIdx][charIdx].hit = true
-					
-					if (charIdx + 1 !== words[wordIdx].length) {
-						charIdx++
-					} else if (wordIdx + 1 !== words.length) {
-						wordIdx++
-						charIdx = 0
-					}
-				} else {
-					words[wordIdx].splice(charIdx, 0, {
-						char: e.key, hit: false, error: true
-					})
-					charIdx++
-					words = words
-				}
+		if (e.key === "Tab") {
+			e.preventDefault()
+			reset()
+			return 
+		}
+
+		if (e.key === words[wordIdx][charIdx].char) {
+			words[wordIdx][charIdx].hit = true
+			words[wordIdx][charIdx].current = false
+			
+			if (charIdx + 1 !== words[wordIdx].length) {
+				charIdx++
+			} else if (wordIdx + 1 !== words.length) {
+				wordIdx++
+				charIdx = 0
+			}
+
+			words[wordIdx][charIdx].current = true
+		} else if (e.key.length === 1) {
+			words[wordIdx].splice(charIdx, 0, pushChar(e.key, true))
+			charIdx++
+			words = words
 		}
 	}
-
-	// TODO
-	// - MOVE AND HIGHLIGHT CURRENT CHAR
-	// - ADD CURSOR
-	// - DISPLAY STATS WHILE TYPING 
-	// - ADD END SCREEN
-	// - ADD SETTINGS
-	//		- ADD TOP WORDS
-	// 		- ADD LANGUAGES
-	//		- (ADD THEMES)
-	// - LIGHTHOUSE
 </script>
 
 <svelte:window on:keydown={handleKeydown} />
 
 <section>
-	{#each words as word, wIdx (wIdx)}
-		<p>
-			{#each word as {char, hit, error}, cIdx (cIdx)}
-				<span class:hit class:error class="char">
-					{#if char === " "}
-						&nbsp 
-					{:else}
-						{char}
-					{/if}
-				</span>
+	<div class="type-test-wrapper">
+		<div class="type-test">
+			{#each words as word, wIdx (wIdx)}
+				<p>
+					{#each word as {char, hit, error, current}, cIdx (cIdx)}
+						<span class:hit class:error class:current class="char">
+							{#if char === " "}
+								&nbsp 
+							{:else}
+								{char}
+							{/if}
+						</span>
+					{/each}
+				</p>
 			{/each}
-		</p>
-	{/each}
+		</div>
+
+		<div class="fade left" />
+		<div class="fade right" />
+		<div class="cursor" />
+	</div>
+
+	<div class="tips">
+		<key>tab</key>
+		- restart test
+	</div>
 </section>
 
 <style lang='scss'>
@@ -112,21 +121,70 @@
 		align-items: center;
 		justify-content: center;
 
-		.char {
-			color: var(--second-text);
-			font-size: 20px;
-			user-select: none;
-			-moz-user-select: none;
-			-webkit-user-select: none;
+		.type-test-wrapper {
+			position: relative;
+			width: 50%;
+			height: 100px;
+			overflow: hidden;
+
+			.fade {
+				position: absolute;
+				top: 0;
+				height: 100%;
+				width: 10%;
+			}
+
+			.left {
+				left: 0;
+				background: linear-gradient(to right, var(--bg) , transparent);
+			}
+
+			.right {
+				right: 0;
+				background: linear-gradient(to left, var(--bg) , transparent);
+			}
+
+			.type-test {
+				display: flex;
+				position: absolute;
+				top: 50%;
+				transform: translateY(-50%);
+				// left: 50%;
+	
+				.char {
+					color: var(--text);
+					font-size: 20px;
+					user-select: none;
+					-moz-user-select: none;
+					-webkit-user-select: none;
+				}
+		
+				.hit {
+					color: var(--second-text) !important;
+				}
+		
+				.error {
+					color: rgb(var(--error-text)) !important;
+					background: rgba( var(--error-text), 0.1);
+				}
+	
+				.current {
+					font-weight: 700;
+				}
+			}
 		}
 
-		.hit {
-			color: var(--text) !important;
-		}
+		.tips {
+			position: fixed;
+			bottom: 72px;
+			font-size: 15px;
 
-		.error {
-			color: rgb(var(--error-text)) !important;
-			background: rgba( var(--error-text), 0.1);
+			key {
+				background: var(--text);
+				color: var(--bg);
+				padding: 1px 5px;
+				font-size: 14px;
+			}
 		}
 	}
 </style>
