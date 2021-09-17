6783ef455344d1e3391035fbca6a9ceadf858846
7USTIN <justin.faak@icloud.com>
2021-09-17 21:06:18 +0200

    center current char

--- src/components/TypingTest.svelte
+++ src/components/TypingTest.svelte
@@ -1,16 +1,35 @@
 <script lang='ts'>
+	import { onMount, tick } from "svelte"
 	import english from "../languages/englishWords"
 
 	interface char {
 		char: string,
 		hit: boolean,
 		error: boolean,
-		current: boolean
+		current: boolean,
+		next: boolean
 	}
 
 	let words: char[][] = getRandomWords(20)
+
+	let typingTestEl: HTMLDivElement
+	let wrapperEl: HTMLDivElement
+	let charOffset = 0
+
 	let charIdx = 0
 	let wordIdx = 0
+
+	$: currentChar = document.getElementsByClassName("current")
+	$: nextChar = document.getElementsByClassName("next")
+	
+	function setOffset() {
+		typingTestEl.style.left = wrapperEl.clientWidth / 2 - charOffset + "px"
+	}
+
+	onMount(() => {
+		charOffset = currentChar[0].scrollWidth / 2
+		setOffset()
+	})
 
 	function getRandomItem(array: any[]): [] {
 		return array[array.length * Math.random() | 0]
@@ -22,10 +41,11 @@
 			error,
 			hit: false,
 			current: false,
+			next: false,
 		}
 	}
 
-	function getRandomWords(numWords: number): [][] { // TODO
+	function getRandomWords(numWords: number): [][] {
 		const array = []
 		const words = []
 
@@ -39,20 +59,26 @@
 			for (let j = 0; j < words[i].length; j++) {
 				array[i].push(pushChar(words[i][j], false))
 			}
-			array[0][0].current = true
 			
 			if (i + 1 !== numWords) {
 				array[i].push(pushChar(" ", false))
 			}
+			
+			array[0][0].current = true
+			array[0][1].next = true
 		}
 
-		console.log(array)
 		return array
 	}
 
-	function reset() {
+	async function reset() {
 		[charIdx, wordIdx] = [0, 0]
 		words = getRandomWords(20)
+
+		await tick()
+
+		charOffset = currentChar[0].scrollWidth / 2
+		setOffset()
 	}
 
 	function handleKeydown(e: KeyboardEvent): void {
@@ -72,25 +98,49 @@
 				wordIdx++
 				charIdx = 0
 			}
-
+			
 			words[wordIdx][charIdx].current = true
+			words[wordIdx][charIdx].next = false
+			
+			if (words[wordIdx][charIdx].char !== " ") {
+				words[wordIdx][charIdx].next = false
+
+				if (wordIdx + 1 !== words.length) {
+					words[wordIdx][charIdx + 1].next = true
+				}
+			} else {
+				words[wordIdx][0].next = false
+				words[wordIdx + 1][0].next = true
+			}
 		} else if (e.key.length === 1) {
 			words[wordIdx].splice(charIdx, 0, pushChar(e.key, true))
 			charIdx++
 			words = words
 		}
+
+		charOffset += nextChar[0].scrollWidth
+		setOffset()
 	}
 </script>
 
-<svelte:window on:keydown={handleKeydown} />
+<svelte:window on:keydown={handleKeydown} on:resize={setOffset} />
 
 <section>
-	<div class="type-test-wrapper">
-		<div class="type-test">
+	<div class="typing-test-wrapper" bind:this={wrapperEl}>
+		<div 
+			class="typing-test" 
+			bind:this={typingTestEl}
+		>
 			{#each words as word, wIdx (wIdx)}
 				<p>
-					{#each word as {char, hit, error, current}, cIdx (cIdx)}
-						<span class:hit class:error class:current class="char">
+					{#each word as {char, hit, error, current, next}, cIdx (cIdx)}
+						<span
+							class:hit
+							class:error
+							class:current
+							class:next
+							class="char"
+						>
 							{#if char === " "}
 								&nbsp 
 							{:else}
@@ -104,7 +154,6 @@
 
 		<div class="fade left" />
 		<div class="fade right" />
-		<div class="cursor" />
 	</div>
 
 	<div class="tips">
@@ -121,11 +170,24 @@
 		align-items: center;
 		justify-content: center;
 
-		.type-test-wrapper {
+		.typing-test-wrapper {
 			position: relative;
 			width: 50%;
 			height: 100px;
 			overflow: hidden;
+
+			&::after {
+				content: "";
+				position: absolute;
+				left: 50%;
+				top: calc(50% + 15px);
+				transform: translate(-50%, -50%);
+				width: 0; 
+				height: 0; 
+				border-left: 5px solid transparent;
+				border-right: 5px solid transparent;
+				border-bottom: 5px solid var(--second-text);
+			}
 
 			.fade {
 				position: absolute;
@@ -144,12 +206,11 @@
 				background: linear-gradient(to left, var(--bg) , transparent);
 			}
 
-			.type-test {
+			.typing-test {
 				display: flex;
 				position: absolute;
 				top: 50%;
 				transform: translateY(-50%);
-				// left: 50%;
 	
 				.char {
 					color: var(--text);
